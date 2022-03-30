464d5e888c9be1908976a301cf6ee789dbfbe35e
GCastilho <castilho.gabriel14@gmail.com>
2022-03-30 16:03:06 -0300

    Rota root agora retorna o ipca acumulado de 12 meses

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+import pandas as pd
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+ipca = pd.read_excel('ipca_com_tratativa.xlsx', parse_dates=['data']).sort_values(by='data').tail(12)['valor'].sum()
+
 @app.route('/')
 def root():
-	return 'Hello World'
+	return jsonify({
+		'ipca': ipca
+	})
 
 app.run()
