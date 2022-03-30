a37a6d22962bd02b33403e191d93dc0bf91990fb
GCastilho <castilho.gabriel14@gmail.com>
2022-03-30 16:06:12 -0300

    Atualizado para utilizar o xslx do ipca ao invés do ipca com tratativa

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-ipca = pd.read_excel('ipca_com_tratativa.xlsx', parse_dates=['data']).sort_values(by='data').tail(12)['valor'].sum()
+ipca = pd.read_excel('ipca.xlsx', parse_dates=['data']).sort_values(by='data').tail(12)['valor'].sum()
+ipca = round(ipca, 2)
 
 @app.route('/')
 def root():
